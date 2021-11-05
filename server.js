2109c002fa9dc5823f6bf5e02cbe365059c28e7c
calyun <cmyounk@gmail.com>
2021-11-05 19:17:54 -0400

    turned into express app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+let app = express();
+let PORT = process.env.PORT || 3003;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => console.log("App listening on PORT http://localhost:" + PORT));
